23efe50bc070a3969558f143f1512472c61264f7
Karen V <karenvo@usc.edu>
2019-01-31 17:01:33 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,12 +1,17 @@
 def turn_count(board)
   counter = 0
-  board.each do |count|
-    counter += 1
+  board.each do |turn|
+    if turn.downcase == "x" || turn.downcase == "o"
+      counter += 1
+      puts "#{counter}"
+    end
   end
-end
 
+  counter
+end
+###########################################################
 def current_player
-  if turn_count == even
+  if 
     current_player = "X"
   else
     current_player = "O"
