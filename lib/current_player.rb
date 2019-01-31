bef14b040d28bd5777c822efd44df97c5e178e5c
Karen V <karenvo@usc.edu>
2019-01-31 17:18:08 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,8 +9,8 @@ def turn_count(board)
   counter
 end
 ###########################################################
-def current_player(counter)
-  if counter.even?
+def current_player(turn_count)
+  if turn_count.even?
     current_player = "O"
   else
     current_player = "X"
